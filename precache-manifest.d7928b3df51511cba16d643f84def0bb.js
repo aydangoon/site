40c3fdc1b621299886754dd954644dd974239e56
AydanG <aydang@seas.upenn.edu>
2020-08-21 12:24:41 -0400

    Updates

---
+++ precache-manifest.d7928b3df51511cba16d643f84def0bb.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2856d832315cac49b91be54358a1b0",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "bf312a8bd500a8dceea9",
+    "url": "/site/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "35f75f632893052d5ffe",
+    "url": "/site/static/css/main.7402d954.chunk.css"
+  },
+  {
+    "revision": "bf312a8bd500a8dceea9",
+    "url": "/site/static/js/2.0a3fafea.chunk.js"
+  },
+  {
+    "revision": "b6840d1fa4cd3c3e88b7c215b28b2e9e",
+    "url": "/site/static/js/2.0a3fafea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35f75f632893052d5ffe",
+    "url": "/site/static/js/main.674f8ad4.chunk.js"
+  },
+  {
+    "revision": "9f22d856fd4b9697e2e0",
+    "url": "/site/static/js/runtime-main.50cf3253.js"
+  },
+  {
+    "revision": "d8196ab2e8158c4cf9a76f47b58572fb",
+    "url": "/site/static/media/2dboids.d8196ab2.png"
+  },
+  {
+    "revision": "88be4e42c2b93f182424d111a549eea9",
+    "url": "/site/static/media/3dboids.88be4e42.gif"
+  },
+  {
+    "revision": "3335892171702c2f4eba4d77e9416b3c",
+    "url": "/site/static/media/HOL.33358921.png"
+  },
+  {
+    "revision": "2fe27d9d10cdfccb1baef28a45d5ba90",
+    "url": "/site/static/media/Lato-Light.2fe27d9d.ttf"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "/site/static/media/Ubuntu-Regular.2505bfbd.ttf"
+  },
+  {
+    "revision": "123d8167545b75130aa4f902a0981622",
+    "url": "/site/static/media/ascher.123d8167.jpg"
+  },
+  {
+    "revision": "1de10e2e01c2059d0f6076ac91cc9df8",
+    "url": "/site/static/media/aydang_resume.1de10e2e.pdf"
+  },
+  {
+    "revision": "46234b06aa50b0dc5eeda6ebee0fd6ae",
+    "url": "/site/static/media/batesian.46234b06.png"
+  },
+  {
+    "revision": "8a18f73eb2b5fa3e6caf8e822497555a",
+    "url": "/site/static/media/fullstack.8a18f73e.png"
+  },
+  {
+    "revision": "dfe57f30d3bd0722b82101752d1c193a",
+    "url": "/site/static/media/gravity.dfe57f30.png"
+  },
+  {
+    "revision": "fae77f12f9506ee572d26896bad25103",
+    "url": "/site/static/media/hamburger.fae77f12.png"
+  },
+  {
+    "revision": "8e995e1c5cc40f9205dc09d1cf8a2e92",
+    "url": "/site/static/media/lithegl.8e995e1c.png"
+  },
+  {
+    "revision": "c4182790027a5c48196247c9c100062b",
+    "url": "/site/static/media/me.c4182790.jpg"
+  },
+  {
+    "revision": "05da7774017cffd7d61835e5cde6234d",
+    "url": "/site/static/media/music.05da7774.jpg"
+  },
+  {
+    "revision": "ac9e34b095f1121e027492f2f9f2483d",
+    "url": "/site/static/media/snake.ac9e34b0.png"
+  },
+  {
+    "revision": "e308b3d3c10b7908f00194f7f0416f10",
+    "url": "/site/static/media/werewolf.e308b3d3.png"
+  }
+]);
