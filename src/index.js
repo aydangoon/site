705a293836c2604e8adf55856f0e821842442aa9
AydanG <aydang@seas.upenn.edu>
2020-08-21 12:56:33 -0400

    basepath changed

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import App from './App';
 
 ReactDOM.render(
     <React.StrictMode>
-        <Router basename='/site'>
+        <Router>
             <App />
         </Router>
     </React.StrictMode>,
